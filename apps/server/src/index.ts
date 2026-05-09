3949a112799e03d727d26a2aacd7769325f8ea7e
DatTTSE172775 <datttse172775@fpt.edu.vn>
2026-05-09 15:50:35 +0700

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,38 @@
+import { cors } from "@elysiajs/cors";
+import { node } from "@elysiajs/node";
+import { createContext } from "@engducation/api/context";
+import { appRouter } from "@engducation/api/routers/index";
+import { auth } from "@engducation/auth";
+import { env } from "@engducation/env/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { Elysia } from "elysia";
+
+new Elysia({ adapter: node() })
+  .use(
+    cors({
+      origin: env.CORS_ORIGIN,
+      methods: ["GET", "POST", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }),
+  )
+  .all("/api/auth/*", async (context) => {
+    const { request, status } = context;
+    if (["POST", "GET"].includes(request.method)) {
+      return auth.handler(request);
+    }
+    return status(405);
+  })
+  .all("/trpc/*", async (context) => {
+    const res = await fetchRequestHandler({
+      endpoint: "/trpc",
+      router: appRouter,
+      req: context.request,
+      createContext: () => createContext({ context }),
+    });
+    return res;
+  })
+  .get("/", () => "OK")
+  .listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+  });
---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@engducation/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
